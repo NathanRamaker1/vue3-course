cfe60958b305e14569e010b6daf4617343aba7dc
Nathan Ramaker <r0933325@student.thomasmore.be>
2025-02-01 16:26:00 +0100

    Started new recipe book project with vue router - Section 11

---
+++ 6thMediumProject/recipe-book/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+
+        <li>
+          <RouterLink :to="{name: 'home'}">Home</RouterLink>
+        </li>
+        <li>
+          <RouterLink :to="{name: 'favorites'}">Favorites</RouterLink>
+        </li>
+        <li>
+          <RouterLink :to="{name: 'add-recipe'}">Add Recipe</RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import {  RouterLink, RouterView } from 'vue-router';
+
+</script>
+
+
---
+++ 6thMediumProject/recipe-book/src/views/RecipeView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        Recipe {{ $route.params.id }}
+    </div>
+    <nav>
+        <ul>
+            <li>
+                <RouterLink :to="{name: 'recipe', params: {id: 1}}">Recipe 1</RouterLink>
+            </li>
+            <li>
+                <RouterLink :to="{name: 'recipe', params: {id: 3}}">Recipe 3</RouterLink>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script setup lang="ts">
+    import { onMounted, watch } from 'vue';
+import { useRoute,RouterLink } from 'vue-router';
+
+    const route = useRoute();
+
+    console.log(route.params.id);
+ 
+    watch(() => route.params.id, () => console.log('Fetching watch'), {immediate: true});
+
+</script>
